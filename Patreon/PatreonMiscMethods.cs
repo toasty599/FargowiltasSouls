dbc9ecd0efaf59fc9c7ee141cfeec4234c994a95
Pbone3 <pbone785@gmail.com>
2021-04-17 23:28:44 -0500

    Legs working, body WIP

--- Patreon/PatreonMiscMethods.cs
+++ Patreon/PatreonMiscMethods.cs
@@ -7,7 +7,7 @@ namespace FargowiltasSouls.Patreon
         public static void Load(Mod mod)
         {
             mod.AddEquipTexture(null, EquipType.Legs, "BetaLeg", "FargowiltasSouls/Patreon/JojoTheGamer/Beta_Legs");
-            mod.AddEquipTexture(null, EquipType.Body, "BetaBody", "FargowiltasSouls/Patreon/JojoTheGamer/Beta_Body");
+            mod.AddEquipTexture(null, EquipType.Body, "BetaBody", "FargowiltasSouls/Patreon/JojoTheGamer/Beta_Body", "FargowiltasSouls/Patreon/JojoTheGamer/Beta_Arms");
             mod.AddEquipTexture(null, EquipType.Head, "BetaHead", "FargowiltasSouls/Patreon/JojoTheGamer/Beta_Head");
         }
     }
