a2d843d66f33db660608959fcd25a0c7a98f8c1e
terrynmuse <darius.loyhrs@hotmail.com>
2021-05-10 11:28:35 -0400

    fixed spider ench not working during mutant

--- NPCs/FargoSoulsGlobalNPC.cs
+++ NPCs/FargoSoulsGlobalNPC.cs
@@ -952,7 +952,7 @@ namespace FargowiltasSouls.NPCs
             /*if (modPlayer.BeeEnchant && !modPlayer.TerrariaSoul && projectile.type == ProjectileID.GiantBee)
                 damage = (int)(damage + npc.defense * .5);*/
 
-            if (modPlayer.SpiderEnchant && projectile.minion && Main.rand.Next(101) <= modPlayer.SummonCrit && player.GetToggleValue("Spider"))
+            if (modPlayer.SpiderEnchant && projectile.minion && Main.rand.Next(101) <= modPlayer.SummonCrit && player.GetToggleValue("Spider", false))
             {
                 /*if (modPlayer.LifeForce || modPlayer.WizardEnchant)
                 {
