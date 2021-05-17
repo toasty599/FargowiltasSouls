a9a85ae400e0eb1bae25d49a0d7e4fd63f465821
terrynmuse <darius.loyhrs@hotmail.com>
2021-05-17 12:08:40 -0400

    penetrator dive nuke visual tweak

--- Projectiles/BossWeapons/HentaiNuke.cs
+++ Projectiles/BossWeapons/HentaiNuke.cs
@@ -72,6 +72,64 @@ namespace FargowiltasSouls.Projectiles.BossWeapons
 
                 if (!Main.dedServ)
                     Main.PlaySound(mod.GetLegacySoundSlot(SoundType.Custom, "Sounds/Thunder").WithVolume(0.8f).WithPitchVariance(-0.5f), projectile.Center);
+
+                for (int a = 0; a < 4; a++)
+                {
+                    for (int index1 = 0; index1 < 3; ++index1)
+                    {
+                        int index2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 31, 0.0f, 0.0f, 100, new Color(), 1.5f);
+                        Main.dust[index2].position = new Vector2((float)(projectile.width / 2), 0.0f).RotatedBy(6.28318548202515 * Main.rand.NextDouble(), new Vector2()) * (float)Main.rand.NextDouble() + projectile.Center;
+                    }
+                    for (int index1 = 0; index1 < 10; ++index1)
+                    {
+                        int index2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 229, 0.0f, 0.0f, 0, new Color(), 2.5f);
+                        Main.dust[index2].position = new Vector2((float)(projectile.width / 2), 0.0f).RotatedBy(6.28318548202515 * Main.rand.NextDouble(), new Vector2()) * (float)Main.rand.NextDouble() + projectile.Center;
+                        Main.dust[index2].noGravity = true;
+                        Dust dust1 = Main.dust[index2];
+                        dust1.velocity = dust1.velocity * 1f;
+                        int index3 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 229, 0.0f, 0.0f, 100, new Color(), 1.5f);
+                        Main.dust[index3].position = new Vector2((float)(projectile.width / 2), 0.0f).RotatedBy(6.28318548202515 * Main.rand.NextDouble(), new Vector2()) * (float)Main.rand.NextDouble() + projectile.Center;
+                        Dust dust2 = Main.dust[index3];
+                        dust2.velocity = dust2.velocity * 1f;
+                        Main.dust[index3].noGravity = true;
+                    }
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 229, 0f, 0f, 100, default, 3f);
+                        Main.dust[dust].velocity *= 1.4f;
+                    }
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 6, 0f, 0f, 100, default, 3.5f);
+                        Main.dust[dust].noGravity = true;
+                        Main.dust[dust].velocity *= 7f;
+                        dust = Dust.NewDust(projectile.position, projectile.width, projectile.height, 6, 0f, 0f, 100, default, 1.5f);
+                        Main.dust[dust].velocity *= 3f;
+                    }
+
+                    for (int index1 = 0; index1 < 10; ++index1)
+                    {
+                        int index2 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 229, 0f, 0f, 100, new Color(), 2f);
+                        Main.dust[index2].noGravity = true;
+                        Main.dust[index2].velocity *= 21f * projectile.scale;
+                        Main.dust[index2].noLight = true;
+                        int index3 = Dust.NewDust(projectile.position, projectile.width, projectile.height, 229, 0f, 0f, 100, new Color(), 1f);
+                        Main.dust[index3].velocity *= 12f;
+                        Main.dust[index3].noGravity = true;
+                        Main.dust[index3].noLight = true;
+                    }
+
+                    for (int i = 0; i < 10; i++)
+                    {
+                        int d = Dust.NewDust(projectile.position, projectile.width, projectile.height, 229, 0f, 0f, 100, default, Main.rand.NextFloat(2f, 3.5f));
+                        if (Main.rand.Next(3) == 0)
+                            Main.dust[d].noGravity = true;
+                        Main.dust[d].velocity *= Main.rand.NextFloat(9f, 12f);
+                        Main.dust[d].position = Main.player[projectile.owner].Center;
+                    }
+                }
             }
         }
 
@@ -83,7 +141,7 @@ namespace FargowiltasSouls.Projectiles.BossWeapons
 
         public override Color? GetAlpha(Color lightColor)
         {
-            return new Color(255, 255, 255, 64);
+            return new Color(255, 255, 255, 200);
         }
 
         public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
