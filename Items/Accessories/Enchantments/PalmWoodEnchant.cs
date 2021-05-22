37b78c5b203863d6b246ab699e4a10d7989a171c
Lin <63439268+Lin-cat@users.noreply.github.com>
2021-05-22 09:38:49 +0800

    Update PalmWoodEnchant.cs

--- Items/Accessories/Enchantments/PalmWoodEnchant.cs
+++ Items/Accessories/Enchantments/PalmWoodEnchant.cs
@@ -17,7 +17,7 @@ namespace FargowiltasSouls.Items.Accessories.Enchantments
 'Alarmingly calm'");
             DisplayName.AddTranslation(GameCulture.Chinese, "棕榈木魔石");
             Tooltip.AddTranslation(GameCulture.Chinese, 
-@"双击'下'键会召唤一颗会向敌人扔橡子的棕榈树哨兵
+@"双击'下'键会召唤一个会向敌人扔橡子的棕榈树哨兵
 '出奇的宁静'");
         }
 
