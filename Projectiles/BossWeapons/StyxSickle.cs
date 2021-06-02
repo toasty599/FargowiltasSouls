9541bc5034af54417e26623e66460183fd796af5
terrynmuse <darius.loyhrs@hotmail.com>
2021-06-02 19:56:49 -0400

    w

--- Projectiles/BossWeapons/StyxSickle.cs
+++ Projectiles/BossWeapons/StyxSickle.cs
@@ -130,7 +130,7 @@ namespace FargowiltasSouls.Projectiles.BossWeapons
 
             for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[projectile.type]; i++)
             {
-                Color color27 = color26 * 0.75f;
+                Color color27 = color26 * 0.8f;
                 color27 *= (float)(ProjectileID.Sets.TrailCacheLength[projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[projectile.type];
                 Vector2 value4 = projectile.oldPos[i];
                 float num165 = projectile.oldRot[i];
