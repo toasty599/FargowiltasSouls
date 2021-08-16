ed0b6769b62bc7da0501dd6aefbf222c69bea318
terrynmuse <darius.loyhrs@hotmail.com>
2021-08-15 22:59:08 -0400

    the following debuffs can NEVER be reapplied to you while you already have them (except by auras)  berserked  hexed  lovestruck  purified  stunned  cursed  silenced  frozen  webbed  stoned  distorted fixed life champ spawn anim mp desync

--- Buffs/FargoGlobalBuff.cs
+++ Buffs/FargoGlobalBuff.cs
@@ -86,5 +86,26 @@ namespace FargowiltasSouls.Buffs
                     break;
             }
         }
+
+        public override bool ReApply(int type, Player player, int time, int buffIndex)
+        {
+            if (FargoSoulsWorld.MasochistMode && time > 2)
+            {
+                switch(type)
+                {
+                    case BuffID.Cursed:
+                    case BuffID.Silenced:
+                    case BuffID.Frozen:
+                    case BuffID.Webbed:
+                    case BuffID.Stoned:
+                    case BuffID.VortexDebuff:
+                        return true;
+
+                    default: break;
+                }
+            }
+
+            return base.ReApply(type, player, time, buffIndex);
+        }
     }
 }
