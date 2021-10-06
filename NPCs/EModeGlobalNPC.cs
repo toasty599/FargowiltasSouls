d7ed21c3af696a6ba64e68ffdf5d0065abe94ecf
terrynmuse <darius.loyhrs@hotmail.com>
2021-10-06 19:18:21 -0400

    MAYBE fixed goblin thief and others deleting modded item data?? TEST THIS

--- NPCs/EModeGlobalNPC.cs
+++ NPCs/EModeGlobalNPC.cs
@@ -8246,40 +8246,21 @@ namespace FargowiltasSouls.NPCs
                 target.GetModPlayer<FargoPlayer>().StealingCooldown = 360; //trust me, keep these separate
                 target.AddBuff(ModContent.BuffType<ThiefCD>(), 360);
 
-                int i = Item.NewItem((int)target.position.X, (int)target.position.Y, target.width, target.height, item.type, 1, false, 0, false, false);
+                int i = Item.NewItem((int)target.position.X, (int)target.position.Y, target.width, target.height, item.type, item.stack, false, -1, false, false);
+                Vector2 position = Main.item[i].position;
+
                 Main.item[i] = item.Clone();
                 Main.item[i].whoAmI = i;
-                //Main.item[i].netDefaults(item.netID);
-                //Main.item[i].Prefix(item.prefix);
+                Main.item[i].position = position;
                 Main.item[i].stack = item.stack;
+
                 Main.item[i].velocity.X = Main.rand.Next(-20, 21) * 0.2f;
                 Main.item[i].velocity.Y = Main.rand.Next(-20, 1) * 0.2f;
                 Main.item[i].noGrabDelay = 100;
                 Main.item[i].newAndShiny = false;
-                NetMessage.SendData(MessageID.SyncItem, -1, -1, null, i, 1f);
 
-                /*if (Main.netMode == NetmodeID.MultiplayerClient) //ask server to instance an item
-                {
-                    var netMessage = mod.GetPacket();
-                    netMessage.Write((byte)16);
-                    netMessage.Write(target.whoAmI);
-                    netMessage.Write(item.type);
-                    netMessage.Write(item.netID);
-                    netMessage.Write(item.prefix);
-                    netMessage.Write(item.stack);
-                    netMessage.Send();
-                }
-                else if (Main.netMode == NetmodeID.SinglePlayer)
-                {
-                    int i = Item.NewItem((int)target.position.X, (int)target.position.Y, target.width, target.height, item.type, 1, false, 0, false, false);
-                    Main.item[i].netDefaults(item.netID);
-                    Main.item[i].Prefix(item.prefix);
-                    Main.item[i].stack = item.stack;
-                    Main.item[i].velocity.X = Main.rand.Next(-20, 21) * 0.2f;
-                    Main.item[i].velocity.Y = Main.rand.Next(-20, 1) * 0.2f;
-                    Main.item[i].noGrabDelay = 100;
-                    Main.item[i].newAndShiny = false;
-                }*/
+                if (Main.netMode == NetmodeID.MultiplayerClient)
+                    NetMessage.SendData(MessageID.SyncItem, -1, -1, null, i, 1f);
 
                 item.TurnToAir();
 
