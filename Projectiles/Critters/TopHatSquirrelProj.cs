c2e011b5d609c9efa27b3c9ad816beecaaefab41
terrynmuse <darius.loyhrs@hotmail.com>
2022-03-23 22:34:17 -0400

    top hat squirrel weapon visual tweak

--- Projectiles/Critters/TopHatSquirrelProj.cs
+++ Projectiles/Critters/TopHatSquirrelProj.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -11,12 +12,19 @@ namespace FargowiltasSouls.Projectiles
 
         public override string Texture => "FargowiltasSouls/Items/Weapons/Misc/TophatSquirrelWeapon";
 
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Top Hat Squirrel");
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 15;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
+        }
+
         public override void SetDefaults()
         {
-            Projectile.extraUpdates = 0;
-            Projectile.width = 15;
-            Projectile.height = 17;
+            Projectile.width = 20;
+            Projectile.height = 20;
             Projectile.friendly = true;
+            Projectile.aiStyle = -1;
             //Projectile.penetrate = -1;
             Projectile.DamageType = DamageClass.Magic;
             Projectile.scale = 0.5f;
@@ -25,7 +33,8 @@ namespace FargowiltasSouls.Projectiles
 
         public override void AI()
         {
-            Projectile.rotation += 0.2f;
+            Projectile.spriteDirection = System.Math.Sign(Projectile.velocity.X);
+            Projectile.rotation += 0.2f * Projectile.spriteDirection;
             Projectile.scale += .02f;
         }
 
@@ -54,6 +63,29 @@ namespace FargowiltasSouls.Projectiles
                         Projectile.damage * 4, Projectile.knockBack, Main.myPlayer);
                 }
             }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D texture2D13 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
+            int num156 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value.Height / Main.projFrames[Projectile.type]; //ypos of lower right corner of sprite to draw
+            int y3 = num156 * Projectile.frame; //ypos of upper left corner of sprite to draw
+            Rectangle rectangle = new Rectangle(0, y3, texture2D13.Width, num156);
+            Vector2 origin2 = rectangle.Size() / 2f;
+
+            SpriteEffects effects = Projectile.spriteDirection > 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None;
+
+            for (int i = 0; i < ProjectileID.Sets.TrailCacheLength[Projectile.type]; i += 3)
+            {
+                Color color27 = new Color(93, 255, 241, 0);
+                color27 *= (float)(ProjectileID.Sets.TrailCacheLength[Projectile.type] - i) / ProjectileID.Sets.TrailCacheLength[Projectile.type];
+                Vector2 value4 = Projectile.oldPos[i];
+                float num165 = Projectile.oldRot[i];
+                Main.EntitySpriteDraw(texture2D13, value4 + Projectile.Size / 2f - Main.screenPosition + new Vector2(0, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), color27, num165, origin2, Projectile.scale * 1.1f, effects, 0);
+            }
+
+            Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Projectile.GetAlpha(lightColor), Projectile.rotation, origin2, Projectile.scale, effects, 0);
+            return false;
         }
     }
 }
