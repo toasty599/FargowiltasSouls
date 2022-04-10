b8414c620025c8cda6a56b1cc0c46130c3edc79e
terrynmuse <darius.loyhrs@hotmail.com>
2022-04-10 13:28:54 -0400

    mutant spear spin wont spawn eyes beside you

--- Projectiles/MutantBoss/MutantSpearSpin.cs
+++ Projectiles/MutantBoss/MutantSpearSpin.cs
@@ -62,7 +62,7 @@ namespace FargowiltasSouls.Projectiles.MutantBoss
                     if (++Projectile.localAI[0] > 8)
                     {
                         Projectile.localAI[0] = 0;
-                        if (Main.netMode != NetmodeID.MultiplayerClient)
+                        if (Main.netMode != NetmodeID.MultiplayerClient && Projectile.Distance(Main.player[mutant.target].Center) > 360)
                         {
                             Vector2 speed = Vector2.UnitY.RotatedByRandom(Math.PI / 2) * Main.rand.NextFloat(6f, 9f);
                             if (mutant.Center.Y < Main.player[mutant.target].Center.Y)
