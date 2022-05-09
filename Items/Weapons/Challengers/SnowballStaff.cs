34c0f8ce375194aea6987f295b825eef6b893997
Lastprismer <86086696+Lastprismer@users.noreply.github.com>
2022-05-09 08:02:37 +0800

    better description of snowball staff

--- Items/Weapons/Challengers/SnowballStaff.cs
+++ Items/Weapons/Challengers/SnowballStaff.cs
@@ -15,7 +15,7 @@ namespace FargowiltasSouls.Items.Weapons.Challengers
             Tooltip.SetDefault("Creates a snowball that grows as you roll it\nMust use continuously to sustain snowball\nRight click to recall the snowball to yourself");
 
             DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "雪球法杖");
-            Tooltip.AddTranslation((int)GameCulture.CultureName.Chinese, "制造一个会越滚越大的雪球，你可以通过主动推雪球来滚它\n右键点击可让雪球回到你身上");
+            Tooltip.AddTranslation((int)GameCulture.CultureName.Chinese, "制造一个会越滚越大的雪球，你可以通过主动推雪球来滚它\n长按左键才能维持雪球\n右键点击可让雪球回到你身上");
 
             Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
         }
