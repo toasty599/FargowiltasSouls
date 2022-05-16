024722a6a906d963cb28f6e319687312fc82dcb5
terrynmuse <darius.loyhrs@hotmail.com>
2022-05-16 17:47:12 -0400

    refactored trophies

---
+++ Tiles/Trophies/DeviTrophy.cs
@@ -0,0 +1,9 @@
+using Terraria.ModLoader;
+
+namespace FargowiltasSouls.Tiles.Trophies
+{
+    public class DeviTrophy : BaseTrophy
+    {
+        protected override int ItemType => ModContent.ItemType<Items.Placeables.Trophies.DeviTrophy>();
+    }
+}
