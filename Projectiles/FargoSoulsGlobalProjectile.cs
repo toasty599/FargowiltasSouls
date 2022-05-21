c41c3bbfce44e5d10548d769295e050bdb013df3
terrynmuse <darius.loyhrs@hotmail.com>
2022-05-20 20:36:08 -0400

    tentatively removed a fix, see whether spears extend/stack infinitely with autofire again

--- Projectiles/FargoSoulsGlobalProjectile.cs
+++ Projectiles/FargoSoulsGlobalProjectile.cs
@@ -694,11 +694,11 @@ namespace FargowiltasSouls.Projectiles
             Player player = Main.player[projectile.owner];
             FargoSoulsPlayer modPlayer = player.GetModPlayer<FargoSoulsPlayer>();
 
-            if (projectile.aiStyle == ProjAIStyleID.Spear)
-            {
-                if (player.itemTime == 1 && player.whoAmI == Main.myPlayer) //fix spears stacking when autoreuse
-                    projectile.Kill();
-            }
+            //if (projectile.aiStyle == ProjAIStyleID.Spear)
+            //{
+            //    if (player.itemTime == 1 && player.whoAmI == Main.myPlayer) //fix spears stacking when autoreuse
+            //        projectile.Kill();
+            //}
 
             switch (projectile.type)
             {
