eb54772cf76c6665a67355965c27dd02ec12b25f
terrynmuse <darius.loyhrs@hotmail.com>
2022-05-31 20:44:11 -0400

    tungsten ench only reduces melee speed with actual swords with a true melee hitbox

--- Items/Accessories/Enchantments/TungstenEnchant.cs
+++ Items/Accessories/Enchantments/TungstenEnchant.cs
@@ -53,8 +53,10 @@ Enlarged swords and projectiles deal 10% more damage and have an additional chan
 
             modPlayer.TungstenEnchantActive = true;
 
-            if (!modPlayer.TerrariaSoul)
+            if (!modPlayer.TerrariaSoul && player.HeldItem.damage > 0 && player.HeldItem.DamageType == DamageClass.Melee && !player.HeldItem.noMelee)
+            {
                 modPlayer.Player.GetAttackSpeed(DamageClass.Melee) -= 0.5f;
+            }
         }
 
         public static float TungstenIncreaseWeaponSize(FargoSoulsPlayer modPlayer)
