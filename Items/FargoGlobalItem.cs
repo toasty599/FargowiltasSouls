621685b6bfe3a03596688af69651e5e75c03df30
terrynmuse <darius.loyhrs@hotmail.com>
2022-06-12 15:08:10 -0400

    fixed box of gizmos not actually giving all items autofire

--- Items/FargoGlobalItem.cs
+++ Items/FargoGlobalItem.cs
@@ -106,7 +106,7 @@ namespace FargowiltasSouls.Items
             if (modPlayer.TribalCharm && item.type != ItemID.RodofDiscord && item.fishingPole == 0)
                 return true;
 
-            if (modPlayer.BoxofGizmos && item.damage > 0)
+            if (modPlayer.BoxofGizmos)
                 return true;
 
             return base.CanAutoReuseItem(item, player);
