a3963c3d983134742e0bb54a0f4b7d615cc12ec2
terrynmuse <darius.loyhrs@hotmail.com>
2022-06-12 14:25:02 -0400

    emode ml nerfed, null curse no longer disables dodges

--- Buffs/Masomode/NullificationCurse.cs
+++ Buffs/Masomode/NullificationCurse.cs
@@ -9,7 +9,7 @@ namespace FargowiltasSouls.Buffs.Masomode
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Nullification Curse");
-            Description.SetDefault("You cannot dodge and Moon Lord has cycling damage type immunities!");
+            Description.SetDefault("Moon Lord has cycling damage type immunities!");
             Main.debuff[Type] = true;
             Main.buffNoSave[Type] = true;
             Main.buffNoTimeDisplay[Type] = true;
@@ -17,11 +17,6 @@ namespace FargowiltasSouls.Buffs.Masomode
             Terraria.ID.BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
             DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "无效诅咒");
             Description.AddTranslation((int)GameCulture.CultureName.Chinese, "无法躲避,并且月球领主进行循环伤害免疫!");
-        }
-
-        public override void Update(Player player, ref int buffIndex)
-        {
-            player.GetModPlayer<FargoSoulsPlayer>().noDodge = true;
         }
     }
 }
