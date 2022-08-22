5c7a5497b95baf43acfe0e15944a2404ee6ec88d
terrynmuse <darius.loyhrs@hotmail.com>
2022-08-22 11:49:41 -0400

    fixed nekomi armor visual issues

--- Items/Armor/NekomiHood.cs
+++ Items/Armor/NekomiHood.cs
@@ -19,7 +19,6 @@ namespace FargowiltasSouls.Items.Armor
             Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
             DisplayName.SetDefault("Nekomi Hood");
             Tooltip.SetDefault(@"Increases max number of minions by 1");
-            ArmorIDs.Head.Sets.DrawHatHair[Item.headSlot] = true;
         }
 
         public override void SetDefaults()
