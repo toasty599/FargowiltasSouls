e782db4173922ae3970a2980a595c39f97bbd6b2
terrynmuse <darius.loyhrs@hotmail.com>
2022-08-25 13:39:25 -0400

    berserked also causes -10% DR

--- Buffs/Masomode/Berserked.cs
+++ Buffs/Masomode/Berserked.cs
@@ -9,7 +9,7 @@ namespace FargowiltasSouls.Buffs.Masomode
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Berserked");
-            Description.SetDefault("10% increased damage and speed but you cannot control yourself");
+            Description.SetDefault("Increased offense, decreased defense, and you cannot control yourself");
             Main.debuff[Type] = true;
             Main.pvpBuff[Type] = true;
             DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "狂暴");
@@ -23,6 +23,7 @@ namespace FargowiltasSouls.Buffs.Masomode
             player.GetDamage(DamageClass.Generic) += 0.1f;
             player.GetModPlayer<FargoSoulsPlayer>().Berserked = true;
             player.moveSpeed += 0.1f;
+            player.endurance -= 0.1f;
         }
 
         public override bool ReApply(Player player, int time, int buffIndex)
