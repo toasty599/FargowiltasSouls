88f4a3b0368d7609e21eeffd5f2db9b4c27dfc5e
terrynmuse <darius.loyhrs@hotmail.com>
2022-08-29 10:27:13 -0400

    changed the sound used by true eye ray minion

--- Projectiles/Minions/PhantasmalDeathrayTrueEye.cs
+++ Projectiles/Minions/PhantasmalDeathrayTrueEye.cs
@@ -59,7 +59,8 @@ namespace FargowiltasSouls.Projectiles.Minions
             }
             if (Projectile.localAI[0] == 0f)
             {
-                SoundEngine.PlaySound(new SoundStyle("Terraria/Sounds/Zombie_104") with { Volume = 0.75f, Pitch = 0 }, Projectile.Center);
+                //SoundEngine.PlaySound(new SoundStyle("Terraria/Sounds/Zombie_104") with { Volume = 0.75f, Pitch = 0 }, Projectile.Center);
+                SoundEngine.PlaySound(SoundID.Item157, Projectile.Center);
             }
             float num801 = 0.4f;
             Projectile.localAI[0] += 1f;
