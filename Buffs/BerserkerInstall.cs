53b758f1b705dcd44ca172ca70e6f26b82ee4bfb
terrynmuse <darius.loyhrs@hotmail.com>
2022-09-06 14:59:48 -0400

    agi lens zerker install reduced from 30% offenses up & defenses down to 20% each

--- Buffs/BerserkerInstall.cs
+++ Buffs/BerserkerInstall.cs
@@ -17,8 +17,8 @@ namespace FargowiltasSouls.Buffs
 
         public static void DebuffPlayerStats(Player player)
         {
-            player.endurance -= 0.30f;
-            player.statDefense -= 30;
+            player.endurance -= 0.20f;
+            player.statDefense -= 20;
         }
 
         public override void Update(Player player, ref int buffIndex)
@@ -28,9 +28,9 @@ namespace FargowiltasSouls.Buffs
             player.GetModPlayer<FargoSoulsPlayer>().Berserked = true;
 
             player.GetModPlayer<FargoSoulsPlayer>().AttackSpeed += 0.50f;
-            player.GetDamage(DamageClass.Generic) += 0.30f;
-            player.GetCritChance(DamageClass.Generic) += 30;
-            player.moveSpeed += 0.30f;
+            player.GetDamage(DamageClass.Generic) += 0.20f;
+            player.GetCritChance(DamageClass.Generic) += 20;
+            player.moveSpeed += 0.20f;
 
             player.hasMagiluminescence = true;
             player.noKnockback = true;
