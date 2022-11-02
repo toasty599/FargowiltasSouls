ae70b67f6f47468d339c1e8c08e43ea745f26fc8
terrynmuse <darius.loyhrs@hotmail.com>
2022-11-01 22:11:18 -0400

    betsy dash dash curse debuff length buffed 15->20s

--- Projectiles/BetsyDash.cs
+++ Projectiles/BetsyDash.cs
@@ -96,7 +96,7 @@ namespace FargowiltasSouls.Projectiles
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
-            target.AddBuff(BuffID.BetsysCurse, 900);
+            target.AddBuff(BuffID.BetsysCurse, 20 * 60);
 
             if (Projectile.owner == Main.myPlayer)
                 Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ProjectileID.SolarWhipSwordExplosion, 0, 0f, Main.myPlayer);
