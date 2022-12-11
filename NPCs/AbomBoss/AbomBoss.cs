5d982a24b6dc1dc50ccd5da542807e3ed8ab9951
terrynmuse <darius.loyhrs@hotmail.com>
2022-12-11 15:47:37 -0500

    fixed abom boss ray not being sheeted properly

--- NPCs/AbomBoss/AbomBoss.cs
+++ NPCs/AbomBoss/AbomBoss.cs
@@ -17,6 +17,7 @@ using System.IO;
 using Terraria;
 using Terraria.Audio;
 using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.Creative;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.Graphics.Effects;
 using Terraria.ID;
@@ -293,6 +294,10 @@ namespace FargowiltasSouls.NPCs.AbomBoss
                         NPC.DelBuff(0);
                     if (++NPC.ai[1] > 120)
                     {
+                        //because this breaks the background???
+                        if (Main.GameModeInfo.IsJourneyMode && CreativePowerManager.Instance.GetPower<CreativePowers.FreezeTime>().Enabled)
+                            CreativePowerManager.Instance.GetPower<CreativePowers.FreezeTime>().SetPowerInfo(false);
+
                         if (FargoSoulsWorld.EternityMode && !SkyManager.Instance["FargowiltasSouls:AbomBoss"].IsActive())
                             SkyManager.Instance.Activate("FargowiltasSouls:AbomBoss");
 
--- Projectiles/AbomBoss/AbomSword.cs
+++ Projectiles/AbomBoss/AbomSword.cs
@@ -16,6 +16,7 @@ namespace FargowiltasSouls.Projectiles.AbomBoss
 
         public override void SetStaticDefaults()
         {
+            base.SetStaticDefaults();
             DisplayName.SetDefault("Styx Gazer Blade");
         }
 
