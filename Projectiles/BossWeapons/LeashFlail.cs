5a6cb54cb8fee4e9bf8cf840ef9085889f39e5ec
terrynmuse <darius.loyhrs@hotmail.com>
2023-01-30 20:08:31 -0500

    fixed leash of cthulhu breaking its neck when tungsten ench

--- Projectiles/BossWeapons/LeashFlail.cs
+++ Projectiles/BossWeapons/LeashFlail.cs
@@ -168,7 +168,17 @@ namespace FargowiltasSouls.Projectiles.BossWeapons
                     Main.EntitySpriteDraw(texture, position - Main.screenPosition, sourceRectangle, color2, rotation, origin, 1f, SpriteEffects.None, 0);
                 }
 
-            return true;
+
+            Texture2D texture2D13 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
+            int num156 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value.Height / Main.projFrames[Projectile.type]; //ypos of lower right corner of sprite to draw
+            int y3 = num156 * Projectile.frame; //ypos of upper left corner of sprite to draw
+            Rectangle rectangle = new Rectangle(0, y3, texture2D13.Width, num156);
+            Vector2 origin2 = rectangle.Size() / 2f;
+            Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY),
+                new Microsoft.Xna.Framework.Rectangle?(rectangle), Projectile.GetAlpha(lightColor), Projectile.rotation, origin2,
+                Projectile.scale * 4, SpriteEffects.None, 0);
+
+            return false;
         }
     }
 }
