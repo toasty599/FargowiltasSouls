d9f80c934af660305d9acb5fb045f2b3cb03ab4a
JavyzTaken <jacklukas@outlook.com>
2023-08-29 15:16:03 +0200

    Resprited Eridanus

--- Content/Bosses/AbomBoss/AbomFrostShard.cs
+++ Content/Bosses/AbomBoss/AbomFrostShard.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.Audio;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
