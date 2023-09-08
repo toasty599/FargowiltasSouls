5c78b3148e4009b818c7533c78928c5d2236b8e0
JavyzTaken <jacklukas@outlook.com>
2023-09-08 23:37:20 +0200

    Added main menu theme with logo, background, and music. Update Music Mod to get the music

---
+++ Content/UI/FargoMenuScreen.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.ModLoader;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.Graphics.Effects;
+using FargowiltasSouls.Content.Sky;
+
+namespace FargowiltasSouls.Content.UI
+{
+    public class FargoMenuScreen : ModMenu
+    {
+
+        public override Asset<Texture2D> Logo => ModContent.Request<Texture2D>("FargowiltasSouls/Assets/UI/MenuLogo");
+
+        //public override Asset<Texture2D> SunTexture => ModContent.Request<Texture2D>($"");
+
+        //public override Asset<Texture2D> MoonTexture => ModContent.Request<Texture2D>($"");
+
+        public override int Music => ModLoader.TryGetMod("FargowiltasMusic", out Mod musicMod)
+                ? MusicLoader.GetMusicSlot(musicMod, "Assets/Music/Nein") : MusicID.MenuMusic;
+
+        public override ModSurfaceBackgroundStyle MenuBackgroundStyle => ModContent.GetInstance<MainMenuBackgroundStyle>();
+
+        public override string DisplayName => "Fargo's";
+
+        public override void OnSelected()
+        {
+            //SoundEngine.PlaySound(SoundID.Roar);
+        }
+
+        public override bool PreDrawLogo(SpriteBatch spriteBatch, ref Vector2 logoDrawCenter, ref float logoRotation, ref float logoScale, ref Color drawColor)
+        {
+            //drawColor = Color.Cyan; // Changes the draw color of the logo
+            return true;
+        }
+    }
+}
