2f179e78fe371973c978769f55161634a71affa5
JavyzTaken <jacklukas@outlook.com>
2023-09-29 06:12:32 +0200

    Right click items should now never delete them

--- Common/Utilities/FargoSoulsUtil.cs
+++ Common/Utilities/FargoSoulsUtil.cs
@@ -270,6 +270,7 @@ namespace FargowiltasSouls //lets everything access it without using
 
         public static void ReplaceItem(this Player player, Item itemToReplace, int itemIDtoReplaceWith)
         {
+            bool foundSlot = false;
             for (int i = 0; i < player.inventory.Length; i++)
             {
                 if (player.inventory[i] == itemToReplace)
@@ -279,8 +280,13 @@ namespace FargowiltasSouls //lets everything access it without using
                     newItem.favorited = itemToReplace.favorited;
                     player.inventory[i] = newItem;
                     itemToReplace.active = false;
+                    foundSlot = true;
                     break;
                 }
+            }
+            if (!foundSlot) //didn't find item in normal inventory, drop instead
+            {
+                Item.NewItem(player.GetSource_ItemUse(itemToReplace), player.Center, itemIDtoReplaceWith, prefixGiven: itemToReplace.prefix);
             }
         }
         public static bool NPCInAnyTiles(NPC npc, bool platforms)
