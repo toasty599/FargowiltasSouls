281752bc6e878a2b1d140e14215ebce1784a4369
JavyzTaken <jacklukas@outlook.com>
2023-09-29 10:26:13 +0200

    Baron rotates down faster on death anim

--- Content/Bosses/BanishedBaron/BanishedBaron.cs
+++ Content/Bosses/BanishedBaron/BanishedBaron.cs
@@ -723,7 +723,7 @@ namespace FargowiltasSouls.Content.Bosses.BanishedBaron
         void DeathAnimation()
         {
             NPC.velocity *= 0.96f;
-            RotateTowards(NPC.Center + Vector2.UnitY, 0.5f);
+            RotateTowards(NPC.Center + Vector2.UnitY, 1.2f);
             NPC.velocity.Y += 0.75f;
             if (Collision.SolidCollision(NPC.position, NPC.width, NPC.height) || Wet() || Timer > 240)
             {
