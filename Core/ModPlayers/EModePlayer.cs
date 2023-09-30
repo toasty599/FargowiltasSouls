e5f34c83b611943a76dbd59c3c85633aedfa2e82
JavyzTaken <jacklukas@outlook.com>
2023-09-30 14:10:54 +0200

    man i aint writing all these out

--- Core/ModPlayers/EModePlayer.cs
+++ Core/ModPlayers/EModePlayer.cs
@@ -252,10 +252,10 @@ namespace FargowiltasSouls.Core.ModPlayers
                     {
                         Player.breath -= 1;
                         
-                        if (++MasomodeSpaceBreathTimer > 4) //player.breath is an int so have to do this
+                        if (++MasomodeSpaceBreathTimer > 3) //player.breath is an int so have to do this
                         {
                             MasomodeSpaceBreathTimer = 0;
-                            Player.breath -= 5;
+                            Player.breath -= 2;
                         }
                         
                         if (Player.breath == 0)
