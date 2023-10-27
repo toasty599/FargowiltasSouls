8547238d7742a01a3c28878c68f376339e5a6b8c
JavyzTaken <jacklukas@outlook.com>
2023-10-27 02:54:41 +0200

    Slime Slinging Slasher damage 367 -> 335

--- Content/Items/Weapons/SwarmDrops/SlimeSlingingSlasher.cs
+++ Content/Items/Weapons/SwarmDrops/SlimeSlingingSlasher.cs
@@ -20,7 +20,7 @@ namespace FargowiltasSouls.Content.Items.Weapons.SwarmDrops
 
         public override void SetDefaults()
         {
-            Item.damage = 367;
+            Item.damage = 335;
             Item.DamageType = DamageClass.Melee;
             Item.width = 48;
             Item.height = 64;
