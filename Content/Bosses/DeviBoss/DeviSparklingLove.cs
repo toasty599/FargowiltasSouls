da0a1f27b72c1fb59d2e3c587b5e563bdf47aab6
JavyzTaken <jacklukas@outlook.com>
2023-12-06 22:10:41 +0100

    Deviantt changes: -Rotates in a random direction while performing the Wyvern orb spiral attack -Does less teleports before repeated hammer spirals in Phase 2 -New telegraph for Guardians attack -Increased her movement speed at the start of the mimics attack -Rune wizard outer aura pulls you in instead of inflicting debuffs -Sparkling Love now has proper physics accelerating rotation

--- Content/Bosses/DeviBoss/DeviSparklingLove.cs
+++ Content/Bosses/DeviBoss/DeviSparklingLove.cs
@@ -23,6 +23,7 @@ namespace FargowiltasSouls.Content.Bosses.DeviBoss
             ProjectileID.Sets.TrailingMode[Projectile.type] = 2;
         }
 
+        const int maxTime = 60 * 3;
         public override void SetDefaults()
         {
             Projectile.width = 100;
@@ -30,7 +31,7 @@ namespace FargowiltasSouls.Content.Bosses.DeviBoss
             Projectile.hostile = true;
             Projectile.ignoreWater = true;
             Projectile.tileCollide = false;
-            Projectile.timeLeft = 180;
+            Projectile.timeLeft = maxTime;
             Projectile.alpha = 250;
             Projectile.aiStyle = -1;
             Projectile.penetrate = -1;
@@ -96,7 +97,7 @@ namespace FargowiltasSouls.Content.Bosses.DeviBoss
                 return;
             }
 
-            if (Projectile.timeLeft == 8)
+            if (Projectile.timeLeft == 2)
             {
                 SoundEngine.PlaySound(SoundID.NPCDeath6, Projectile.Center);
                 SoundEngine.PlaySound(SoundID.Item92, Projectile.Center);
@@ -210,6 +211,12 @@ namespace FargowiltasSouls.Content.Bosses.DeviBoss
 
         public override bool PreDraw(ref Color lightColor)
         {
+            Vector2 offset = Vector2.Zero;
+            if (Projectile.timeLeft > 30)
+            {
+                float amount = 4f * (1f-(Projectile.timeLeft / 180f));
+                offset = Main.rand.NextVector2Circular(amount, amount);
+            }
             Texture2D texture2D13 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
             int num156 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value.Height / Main.projFrames[Projectile.type]; //ypos of lower right corner of sprite to draw
             int y3 = num156 * Projectile.frame; //ypos of upper left corner of sprite to draw
@@ -244,9 +251,9 @@ namespace FargowiltasSouls.Content.Bosses.DeviBoss
                 Main.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend, Main.DefaultSamplerState, DepthStencilState.None, RasterizerState.CullCounterClockwise, null, Main.GameViewMatrix.ZoomMatrix);
             }
 
-            Main.EntitySpriteDraw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Projectile.GetAlpha(lightColor), Projectile.rotation, origin2, Projectile.scale, effects, 0);
+            Main.EntitySpriteDraw(texture2D13, Projectile.Center + offset - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Projectile.GetAlpha(lightColor), Projectile.rotation, origin2, Projectile.scale, effects, 0);
             Texture2D texture2D14 = ModContent.Request<Texture2D>("FargowiltasSouls/Content/Items/Weapons/FinalUpgrades/SparklingLove_glow", ReLogic.Content.AssetRequestMode.ImmediateLoad).Value;
-            Main.EntitySpriteDraw(texture2D14, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Color.White * Projectile.Opacity, Projectile.rotation, origin2, Projectile.scale, effects, 0);
+            Main.EntitySpriteDraw(texture2D14, Projectile.Center + offset - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(rectangle), Color.White * Projectile.Opacity, Projectile.rotation, origin2, Projectile.scale, effects, 0);
             return false;
         }
     }
