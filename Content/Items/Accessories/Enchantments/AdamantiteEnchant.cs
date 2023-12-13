d6c8f6fde0f2502bc13fcbdd51b4ce5273fd127d
JavyzTaken <jacklukas@outlook.com>
2023-12-13 17:46:16 +0100

    Fixed adamantite enchant center proj damage scale

--- Content/Items/Accessories/Enchantments/AdamantiteEnchant.cs
+++ Content/Items/Accessories/Enchantments/AdamantiteEnchant.cs
@@ -100,7 +100,7 @@ namespace FargowiltasSouls.Content.Items.Accessories.Enchantments
             }
             else
             {
-                projectile.damage = (int)(projectile.damage / 3f);
+                projectile.damage = (int)(projectile.damage / adaDamageRatio);
             }
         }
 
