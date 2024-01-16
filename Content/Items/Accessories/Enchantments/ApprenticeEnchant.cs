24c2acc36b800e87dd3570fe77b71ade532331ed
JavyzTaken <jacklukas@outlook.com>
2024-01-16 12:40:00 +0100

    Fixed Apprentice Enchant interaction with Rainbow Gun

--- Content/Items/Accessories/Enchantments/ApprenticeEnchant.cs
+++ Content/Items/Accessories/Enchantments/ApprenticeEnchant.cs
@@ -3,6 +3,7 @@ using FargowiltasSouls.Core.ModPlayers;
 using FargowiltasSouls.Core.Toggler;
 using FargowiltasSouls.Core.Toggler.Content;
 using Microsoft.Xna.Framework;
+using System.Linq;
 using System.Reflection;
 using Terraria;
 using Terraria.ID;
@@ -177,6 +178,11 @@ namespace FargowiltasSouls.Content.Items.Accessories.Enchantments
                                 item2.stack--;
                             }
                             modPlayer.ApprenticeItemCDs[j] = item2.useAnimation * 4;
+                            if (projToShoot == ProjectileID.RainbowFront || projToShoot == ProjectileID.RainbowBack)
+                            {
+                                foreach (Projectile rainbow in Main.projectile.Where(p => (p.TypeAlive(ProjectileID.RainbowFront) || p.TypeAlive(ProjectileID.RainbowBack)) && p.owner == player.whoAmI))
+                                    rainbow.Kill(); 
+                            }
                             int p = Projectile.NewProjectile(player.GetSource_ItemUse(item), pos, Vector2.Normalize(velocity) * speed, projToShoot, damage, KnockBack, player.whoAmI);
                             Projectile proj = Main.projectile[p];
 
