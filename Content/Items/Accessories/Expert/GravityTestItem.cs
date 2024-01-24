38b3e04f22a000d052e435f67ca034a9cd06ec44
JavyzTaken <jacklukas@outlook.com>
2024-01-24 20:29:36 +0100

    Prime Soul item no longer loads (unfinished)

--- Content/Items/Accessories/Expert/GravityTestItem.cs
+++ Content/Items/Accessories/Expert/GravityTestItem.cs
@@ -1,4 +1,5 @@
 ﻿using Fargowiltas.Items.Tiles;
+using FargowiltasSouls.Content.Items.Accessories.Enchantments;
 using FargowiltasSouls.Content.Items.Accessories.Masomode;
 using FargowiltasSouls.Content.Items.Materials;
 using FargowiltasSouls.Content.Items.Placables;
@@ -12,9 +13,9 @@ using Terraria.ModLoader;
 
 namespace FargowiltasSouls.Content.Items.Accessories.Expert
 {
-    public class PrimeSoul : SoulsItem
+    public class GravityTestItem : SoulsItem
     {
-        public override bool IsLoadingEnabled(Mod mod) => false;
+        public override bool IsLoadingEnabled(Mod mod) => true;
         public override string Texture => "FargowiltasSouls/Content/Items/Placeholder";
         public override void SetStaticDefaults()
         {
@@ -33,16 +34,9 @@ namespace FargowiltasSouls.Content.Items.Accessories.Expert
             Item.expert = true;
         }
 
-        void PrimeSoulEffect(Player player)
-        {
-            FargoSoulsPlayer modPlayer = player.FargoSouls();
-            if (player.AddEffect<PrimeSoulEffect>(Item))
-                modPlayer.PrimeSoulActive = modPlayer.PrimeSoulActiveBuffer = true;
-        }
 
-        public override void UpdateInventory(Player player) => PrimeSoulEffect(player);
-        public override void UpdateVanity(Player player) => PrimeSoulEffect(player);
-        public override void UpdateAccessory(Player player, bool hideVisual) => PrimeSoulEffect(player);
+
+        public override void UpdateAccessory(Player player, bool hideVisual) => player.AddEffect<VortexProjGravity>(Item);
 
         public override void AddRecipes()
         {
@@ -62,17 +56,6 @@ namespace FargowiltasSouls.Content.Items.Accessories.Expert
             .AddTile<CrucibleCosmosSheet>()
 
             .Register();
-        }
-    }
-    public class PrimeSoulEffect : AccessoryEffect
-    {
-        public override Header ToggleHeader => null;
-        public override void PostUpdateEquips(Player player)
-        {
-            if (player.whoAmI == Main.myPlayer && player.ownedProjectileCounts[ModContent.ProjectileType<MinosPrime>()] < 1)
-            {
-                FargoSoulsUtil.NewSummonProjectile(GetSource_EffectItem(player), player.Center, Vector2.Zero, ModContent.ProjectileType<MinosPrime>(), 30, 10f, Main.myPlayer);
-            }
         }
     }
 }
