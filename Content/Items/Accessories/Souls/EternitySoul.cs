ecf90b3a28c39d1f4f04e52285574c1f479b28c2
Furgo <furgos@qq.com>
2024-02-12 02:12:56 +0800

    remove SoE commented tooltips todo: EternitySoul additional tooltip filter tweak

--- Content/Items/Accessories/Souls/EternitySoul.cs
+++ Content/Items/Accessories/Souls/EternitySoul.cs
@@ -37,49 +37,6 @@ namespace FargowiltasSouls.Content.Items.Accessories.Souls
             base.SetStaticDefaults();
 
             WingSlotID = Item.wingSlot;
-            // DisplayName.SetDefault("Soul of Eternity");
-
-            //oh no idk even for translate
-            //put the string array in localization files
-            // TODO: localization
-//             string tooltip_sp = @"'Mortal o Inmortal, todas las cosas reconocen tu reclamación a la divinidad'
-// Drasticamente incrementa regeneración de vida, incrementa tu mana máximo a 999, súbditos por 30, torretas por 20, vida maxima por 500%, reducción de daño por 50%
-// 250% daño incrementado y velocidad de ataque; 100% velocidad de disparo y retroceso; Incrementa penetración de armadura por 50; Críticos hacen 10x daño y la probabilidad de Crítico se vuelve 50%
-// Consigue un crítico para incrementarlo por 10%, a 100% cada ataque gana 10% robo de vida y ganas +10% daño y +10 defensa; Esto se apila hasta 200,000 veces hasta que te golpeen
-// Todos los ataques inflijen Llamas del Universo, Sadismo, Midas y reduce inmunidad de retroceso de los enemigos
-// Invoca estalactitas, un cristal de hojas, espada y escudo benditos, escarabajos, varias mascotas, bolas de fuego de oricalco y todos los jefes del Modo Masoquista a tu lado
-// Ataques pueden crear rayos, pétalos, orbes espectrales, un Guardián de la mazmorra, bolas de nieve, lanzas, o potenciadores
-// Ataques provocan regeneración de vida incrementada, Sombra de Esquivo, explociones de llamas y lluvias de meteoros
-// Projectiles pueden dividirse o quebrarse, tamaño de objetos y projectiles incrementado, ataques crean ataques adicionales y corazones
-// Dejas un rastro de fuego y arcoirises; Enemigos cercanos son incinerados y súbditos escupen guadañas ocasionalmente
-// Animales tienen defensa incrementada y sus almas te ayudarán; Enemigos explotan en agujas; Mejora todas las torretas DD2 grandemente
-// Tocar dos veces abajo para invocar una torreta, llamar a una tormenta antigua, activar sigilo, invocar un portal, y dirigir tu guardián
-// Click derecho para Defender; Presiona la Llave dorada para encerrarte en oro; Presiona la Llave congelada para congelar el tiempo por 5 segundos
-// Escudo de bengala solar te permite embestir, embestir bloques sólidos te teletransporta a través de ellos; Tira una bomba de huma para teletransportarte a ella y obtener el buff de primer golpe
-// Ser golpeado refleja el daño, suelta una exploción de esporas, inflije super sangrado, puede chillar y causa que erupciones en varias cosas cuando seas dañado
-// Otorga regeneración carmesí, inmunidad al fuego, daño por caída, y lava, duplica la colección de hierbas
-// 50% probabilidad de Abejas gigantes, 15% probabilidad de críticos de súbditos, 20% probabilidad de botín extra
-// Otorga inmunidad a la mayoría de estados alterados; Permite velocidad Supersónica y vuelo infinito; Incrementa poder de pesca substancialmente y todas las cañas de pescar tienen 10 señuelos extra
-// Revives 10x más rapido; Evita invocación de jefes, incrementa generación de enemigos, reduce la hostilidad de esqueletos fuera de la mazmorra y fortalece a Cute Fishron
-// Otorga ataque continuo, protección de modificadores, control de gravedad, caída rápida, e inmunidad a retroceso, todos los estados alterados del Modo Masoquista, mejora ganchos y más
-// Incrementa velocidad de colocación de bloques y paredes por 50%, Casi infinito alcance de colocación de bloques y alcance de minar, Velocidad de minería duplicada
-// Invoca un anillo de la muerte inpenetrable alrededor de tí y tu reflejas todos los projectiles; Cuando mueres, explotas y revives con vida al máximo
-// Efectos del Guantelete de fuego, Bolsa yoyó, Mira de francotirador, Esposas celestiales, Flor de maná, Cerebro de confusión, Velo estelar, Collar del cariño, y Capa de abejas
-// Efectos del Saco de esporas, Escudo de paladín, Caparazón de tortuga congelado, Equipo de buceo ártico, Anca de rana, Alfombra voladora, Katiuskas de lava, y Bolsa de aparejos de pescador
-// Efectos del Spray de pintura, Pulsificador, Móvil, Globo gravitacional, Botas floridas, Equipo de maestro ninja, Anillo codicioso, Caparazón celestial, y Piedra brillante
-// Efectos de pociones de Brillo, Espeleólogo, Cazador, y Sentido del peligro; Efectos del Modo Constructor, Reliquia del Infinito y atraes objectos desde más lejos";
-//
-//             //DisplayName.AddTranslation((int)GameCulture.CultureName.Chinese, "永恒之魂");
-//             DisplayName.AddTranslation((int)GameCulture.CultureName.Spanish, "Alma de la Eternidad");
-//             Tooltip.AddTranslation((int)GameCulture.CultureName.Spanish, tooltip_sp);
-
-            /* Tooltip.SetDefault(
-@"'Mortal or Immortal, all things acknowledge your claim to divinity'
-Crit chance is set to 50%
-Crit to increase it by 10%
-At 100% every attack gains 10% life steal
-You also gain +5% damage and +5 defense
-This stacks up to 950 times until you get hit"); */
 
             Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(6, 10));
             ItemID.Sets.AnimatesAsSoul[Item.type] = true;
@@ -245,6 +202,12 @@ This stacks up to 950 times until you get hit"); */
     {
         public static List<string> Tooltips = new();
         public static List<string> TooltipLines = new();
+        public override void OnLocalizationsLoaded() // rebuild on language changed
+        {
+            Tooltips.Clear();
+            PostAddRecipes();
+        }
+
         public override void PostAddRecipes()
         {
             foreach (Recipe recipe in Main.recipe.Where(r => r.createItem != null && r.createItem.ModItem is BaseSoul))
@@ -253,10 +216,10 @@ This stacks up to 950 times until you get hit"); */
                 {
                     if (item.ModItem is ModItem modItem)
                     {
-                        var tooltips = modItem.Tooltip.Value.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+                        var tooltips = modItem.Tooltip.Value.Split('\n', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries); // trim lines start with whitespace
                         Tooltips.AddRange(tooltips);
                     }
-                        
+
                 }
             }
         }
