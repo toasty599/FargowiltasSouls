263b07c9b442202c5e594dbbe3c588280f17c4af
Toasty <louscliff@gmail.com>
2024-02-20 08:35:40 +0000

    - Added SecondsToFrames utility for readability.

---
+++ Common/Utilities/MathematicalUtilities.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FargowiltasSouls
+{
+	public static partial class FargoSoulsUtil
+	{
+		/// <summary>
+		/// Converts the provided seconds amount to the equivalent time in frames (60 per second).
+		/// </summary>
+		/// <param name="timeInSeconds">The amount to convert.</param>
+		/// <returns>The converted amount of time.</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static float SecondsToFrames(float timeInSeconds) => timeInSeconds * 60f;
+	}
+}
